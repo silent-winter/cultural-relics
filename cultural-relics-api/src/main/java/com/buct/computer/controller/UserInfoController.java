42c74b46a1afdd1772bac53ac6359f96e0325c3b
yuechenlong <yuechenlong@bytedance.com>
2022-04-25 21:12:57 +0800

    登录相关功能润色

--- cultural-relics-api/src/main/java/com/buct/computer/controller/UserInfoController.java
+++ cultural-relics-api/src/main/java/com/buct/computer/controller/UserInfoController.java
@@ -11,6 +11,9 @@ import com.buct.computer.service.IUserBrowseLogService;
 import com.buct.computer.service.IUserInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -46,16 +49,43 @@ public class UserInfoController {
         return ApiResult.success(userInfoDetailVO);
     }
 
+
+    @PatchMapping("/info")
+    @ApiOperation("更新登录态用户信息(传id无效)")
+    public ApiResult<String> updateUserInfo(@RequestBody UserInfo userInfo) {
+        userInfo.setId(StpUtil.getLoginId(0));
+        if (userInfoService.updateById(userInfo)) {
+            return ApiResult.success("update user info successfully");
+        }
+        return ApiResult.fail("fail to update user info");
+    }
+
     @PostMapping("/register")
+    @ApiOperation("新用户注册")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userName", value = "用户名" ,required = true),
+            @ApiImplicitParam(name = "password", value = "密码" ,required = true),
+    })
     public ApiResult<String> register(@RequestBody UserInfo userInfo) {
         if(userInfoService.getOne(new QueryWrapper<UserInfo>().eq("user_name", userInfo.getUserName())) != null) {
             return ApiResult.fail("fail to register: the username has existed");
+        }
+        if (StringUtils.isBlank(userInfo.getType())) {
+            userInfo.setType("user");
+        }
+        if (userInfo.getStatus() == null) {
+            userInfo.setStatus(1);
         }
         userInfoService.save(userInfo);
         return ApiResult.success("register successfully");
     }
 
     @PostMapping("/login")
+    @ApiOperation("登录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userName", value = "用户名" ,required = true),
+            @ApiImplicitParam(name = "password", value = "密码" ,required = true),
+    })
     public ApiResult<String> login(@RequestBody UserInfo userInfo) {
         UserInfo savedAccount = userInfoService.getOne(new QueryWrapper<UserInfo>().eq("user_name", userInfo.getUserName()).eq("password", userInfo.getPassword()));
         if (savedAccount == null) {
@@ -66,6 +96,7 @@ public class UserInfoController {
     }
 
     @GetMapping("/logout")
+    @ApiOperation("注销")
     public ApiResult<String> logout() {
         StpUtil.logout();
         return ApiResult.success("logout successfully");
