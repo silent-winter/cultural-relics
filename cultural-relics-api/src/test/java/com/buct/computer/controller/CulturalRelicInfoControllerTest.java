3bcb73b4cb7b7329ca0c74827795c804b8e94d3e
xinzi <1228604898@qq.com>
2022-05-02 13:24:26 +0800

    feat: 数据清洗

--- cultural-relics-api/src/test/java/com/buct/computer/controller/CulturalRelicInfoControllerTest.java
+++ cultural-relics-api/src/test/java/com/buct/computer/controller/CulturalRelicInfoControllerTest.java
@@ -1,14 +1,15 @@
 package com.buct.computer.controller;
 
 import com.buct.computer.model.CulturalRelicInfo;
-import com.buct.computer.service.impl.CulturalRelicInfoServiceImpl;
+import com.buct.computer.service.ICulturalRelicInfoService;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,7 +22,7 @@ import java.util.List;
 public class CulturalRelicInfoControllerTest {
 
     @Autowired
-    private CulturalRelicInfoServiceImpl culturalRelicInfoService;
+    private ICulturalRelicInfoService culturalRelicInfoService;
 
 
     @Test
@@ -37,6 +38,56 @@ public class CulturalRelicInfoControllerTest {
             culturalRelicInfoList.add(relicInfo);
         }
         culturalRelicInfoService.saveBatch(culturalRelicInfoList);
+    }
+
+    @Test
+    void handleStatus() {
+        List<CulturalRelicInfo> culturalRelicInfoList = culturalRelicInfoService.getBaseMapper().selectList(null);
+        List<CulturalRelicInfo> saved = new ArrayList<>();
+        culturalRelicInfoList.stream().forEach(e -> {
+            String location = e.getLocation();
+            if (StringUtils.startsWithAny(location, "Not on view", "not on view")) {
+                e.setStatus(0);
+                saved.add(e);
+            }
+        });
+        culturalRelicInfoService.updateBatchById(saved);
+    }
+
+    @Test
+    void clear() {
+        List<CulturalRelicInfo> culturalRelicInfoList = culturalRelicInfoService.getBaseMapper().selectList(null);
+        culturalRelicInfoList = culturalRelicInfoList.stream().map(e -> {
+            String artist = e.getArtist();
+            artist = artist.replaceAll(" ", " ");
+            e.setArtist(artist);
+
+            String a = e.getClassification();
+            a = a.replaceAll(" ", " ");
+            e.setClassification(a);
+
+            String b = e.getDetail();
+            b = b.replaceAll(" ", " ");
+            e.setDetail(b);
+
+            String c = e.getDimension();
+            c = c.replaceAll(" ", " ");
+            e.setDimension(c);
+
+            String d = e.getDiscoverTime();
+            d = d.replaceAll(" ", " ");
+            e.setDiscoverTime(d);
+
+            String f = e.getLocation();
+            f = f.replaceAll(" ", " ");
+            e.setLocation(f);
+
+            String g = e.getName();
+            g = g.replaceAll(" ", " ");
+            e.setName(g);
+            return e;
+        }).collect(Collectors.toList());
+        culturalRelicInfoService.updateBatchById(culturalRelicInfoList);
     }
 
 }
