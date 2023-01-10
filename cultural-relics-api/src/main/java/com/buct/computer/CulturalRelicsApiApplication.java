31f5f77ebd9b61323af34c35c37bf5ded21b3be2
xinzi <1228604898@qq.com>
2023-01-10 18:53:23 +0800

    last commit

--- cultural-relics-api/src/main/java/com/buct/computer/CulturalRelicsApiApplication.java
+++ cultural-relics-api/src/main/java/com/buct/computer/CulturalRelicsApiApplication.java
@@ -1,9 +1,11 @@
 package com.buct.computer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.buct.computer.mapper"})
 public class CulturalRelicsApiApplication {
 
     public static void main(String[] args) {
